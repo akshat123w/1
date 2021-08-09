d1613941d21b2e477d114b8b48d94c7234267bcb
akshat123w <86547650+akshat123w@users.noreply.github.com>
2021-08-09 11:18:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function preload(){
+seaImg=loadImage("sea.png");
+shipImg1=loadAnimation();
+shipImg2=loadAnimation();
+shipImg3=loadAnimation();
+shipImg4=loadAnimation();
+}
+
+function setup(){
+  createCanvas(400,400);
+  spritename.addImage(seaImg) ;
+}
+
+function draw() {
+  background("blue");
+ 
+}
